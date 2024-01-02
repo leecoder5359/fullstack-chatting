2b3c19940128e77f9d04d1a9d0131a6449a51021
이호준 <leecoder5359@gmail.com>
2024-01-02 10:20:55 +0900

    (feat): global interceptor 추가

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { setupSwagger } from './config/swagger.config';
+import { setupInterceptors } from './config/interceptor.config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     //setup
     setupSwagger(app);
+    setupInterceptors(app);
 
     await app.listen(process.env.PORT || 3000);
     Logger.log(`Application is running on: ${await app.getUrl()}`);
